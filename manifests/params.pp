4aa1e649f1520c6f1b41b20b9d256271e54417a9
root <root@technerd842c.mylabserver.com>
2019-06-03 09:14:02 +0000

    First commit

---
+++ manifests/params.pp
@@ -0,0 +1,15 @@
+class ntp::params {
+  $package_name       = 'ntp'
+  $package_ensure     = 'present'
+  $config_name        = 'present'
+  $config_file_mode   = '0644'
+  $servers            = '0.centos.pool.ntp.org'
+  $service_ensure     = 'running'
+  $service_enable     = true
+  $service_hasrestart = true
+  $service_hasstatus  = true
+  $service_name       = $facts['os']['family'] ? {
+    'Debian'          => 'ntp',
+    'RedHat'          => 'ntpd',
+  }
+}
